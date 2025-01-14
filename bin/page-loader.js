7ecc3bebeb1cef1e0241912341a99725bb6a5130
Kirill Nevinskiy <k.nevinskiy@tbank.ru>
2025-01-14 17:15:12 +0500

    feat: Создание CLI-скелета и базовая команда

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,1 +1,26 @@
 #!/usr/bin/env node
+
+import { Command } from 'commander';
+import pageLoader from '../src/index.js';
+
+const program = new Command();
+
+program
+  .name('page-loader')
+  .description('CLI utility for downloading web pages and resources.')
+  .version('1.0.0')
+  .option('-o, --output [dir]', 'output directory', process.cwd())
+  .argument('<url>')
+  .action((url, options) => {
+    pageLoader(url, options)
+      .then((filePath) => {
+        console.log(filePath);
+        process.exit(0); // успешное завершение
+      })
+      .catch((err) => {
+        console.error(`Error: ${err.message}`);
+        process.exit(1); // завершение с ошибкой
+      });
+  });
+
+program.parse(process.argv);
